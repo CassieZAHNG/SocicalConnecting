9bd41cfe82b5e625a69d0ae751921bc6889af28b
AzureAD\CassieZhang <cassie.zhang@innovior.com>
2021-07-16 16:46:20 +1000

    create routes

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ connectDB()
 
 app.get('/', (req, res) => res.send('API Running'))
 
+//Define routes
+
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/post', require('./routes/api/post'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/user', require('./routes/api/user'))
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
