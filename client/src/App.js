18630c900a3f1c8fd781cdc92af7fbdde3619b3f
AzureAD\CassieZhang <cassie.zhang@innovior.com>
2021-07-18 18:17:17 +1000

    create profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import { LOGOUT } from './actions/types';
 
 // Redux
